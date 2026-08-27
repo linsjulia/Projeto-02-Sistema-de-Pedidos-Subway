1c7a107bc93173853917d0ab9f1d6b52ef22e1f1
linsjulia <julia.silva465@aluno.cps.sp.gov.br>
2026-08-27 17:49:34 -0300

    fix: corrige classes

--- BuilderMethod.js
+++ BuilderMethod.js
@@ -1,57 +1,5 @@
-// SISTEMAS DE PEDIDOS SUBWAY
-
-class Tamanho{
-    constructor(tamanho){
-        this.tamanho = tamanho;
-    }
-}
-
-class Pao{
-    constructor(tipo){
-        this.tipo = this.tipo;
-    }
-}
-
-class Proteina{
-    constructor(tipo){
-        this.tipo = tipo;
-    }
-}
-
-class Queijo{
-    constructor(tipo){
-        this.tipo = tipo;
-    }
-}
-
-class Aquecido{
-    constructor(aquecido){
-        this.aquecido = aquecido;
-    }
-}
-
-class Salada{
-    constructor(tipo){
-        this.tipo = tipo;
-    }
-}
-
-class Molho{
-    constructor(tipo){
-        this.tipo = tipo;
-    }
-}
-
-class TemperoAdicional{
-    constructor(tipo){
-        this.tipo = tipo;
-    }
-}
-
-// Builder
-
-class LancheBuilder{
-    constructor(){
+class LancheBuilder {
+    constructor() {
         this.tamanho = null;
         this.pao = null;
         this.proteina = null;
@@ -62,49 +10,64 @@ class LancheBuilder{
         this.tempero = null;
     }
 
-    definirTamanho(tipo){
-        this.tamanho = new Tamanho(tipo);
+        definirTamanho(tipo) {
+            this.tamanho = tipo;
+            return this;
+        }
+
+        escolherPao(tipo) {
+            this.pao = tipo;
+            return this;
+        }
+
+        escolherProteina(tipo) {
+            this.proteina = tipo;
+            return this;
+        }
+
+        escolherQueijo(tipo) {
+            this.queijo = tipo;
+            return this;
+        }
+
+        escolherAquecido(tipo) {
+        this.aquecido = tipo;
         return this;
     }
 
-    escolherPao(tipo){
-        this.pao = new Pao(tipo);
+
+        escolherSalada(tipo) {
+        this.salada = tipo;
         return this;
     }
 
-    escolherProteina(tipo){
-        this.proteina = new Proteina(tipo);
+        escolherMolho(tipo) {
+        this.molho = tipo;
         return this;
     }
 
-    adicionarQueijo(tipo){
-        this.queijo = new Queijo(tipo);
+        escolherTempero(tipo) {
+        this.tempero = tipo;
+        return this;
     }
 
-    definirAquecimento(){
-        this.aquecido = new Aquecido(aquecido);
-    }
-
-    adicionarSalada(){
-        this.salada = new Salada(tipo);
-    }
-
-    adicionarMolho(){
-        this.molho = new Molho(tipo);
-    }
-
-    adicionarTempero(){
-        this.tempero = new TemperoAdicional(tipo)
-    }
-
-    construir(){
-        return new Lanche(this.tamanho, this.pao, this.proteina, this.queijo, this.aquecido, this.salada, this.molho, this.tempero);
+    construir() {
+        return new Lanche(
+            this.tamanho,
+            this.pao,
+            this.proteina ?? "Sem proteina",
+            this.queijo ?? "Sem queijo",
+            this.aquecido ?? "Não",
+            this.salada ?? "Sem salada",
+            this.molho ?? "Sem molho",
+            this.tempero ?? "Sem tempero"
+        );
     }
 }
 
-// Construindo o lanche:
-class Lanche{
-    constructor(tamanho, pao, proteina, queijo, aquecido, salada, molho, tempero){
+
+class Lanche {
+    constructor(tamanho, pao, proteina, queijo, aquecido, salada, molho, tempero) {
         this.tamanho = tamanho;
         this.pao = pao;
         this.proteina = proteina;
@@ -115,22 +78,58 @@ class Lanche{
         this.tempero = tempero;
     }
 
-    mostrarDetalhes(){
+    mostrarPedido(){
         console.log(`LANCHE: \n
-            \t Tamanhor: \t\t${this.tamanho.tipo} 
-            \t Estilo: \t\t${this.estilo.estilo} 
-            \t Rodas: \t\t${this.rodas.tamanho}
-            \t Interior: \t\t${this.interior.cor}
-            
+            \t Tamanho do lanche: ${this.tamanho} 
+            \t Tipo de pão: ${this.pao} 
+            \t Tipo de proteina: ${this.proteina}
+            \t Queijo: ${this.queijo}
+            \t Aquecimento do lanche: ${this.aquecido}
+            \t Vegetais e saladas: ${this.salada}
+            \t Molhos: ${this.molho}
+            \t Temperos e adicionais: ${this.tempero}
         `)
     }
 }
 
-// Usando o padrão GoF Criacional - Builder
 
-const carro2 = builder
-    .addMotor('1.0')
-    .addEstilo('Popular')
-    .addRodas(13)
-    .addInterior('Vermelho')
-    .construir()
+
+
+const lanche1 = new LancheBuilder()
+    .definirTamanho("30 cm")
+    .escolherPao("3 Queijos")
+    .escolherProteina("Frango")
+    .escolherQueijo("Cheddar")
+    .escolherSalada("Tomate")
+    .escolherMolho("Barbecue")
+    .construir();
+lanche1.mostrarPedido();
+
+const lanche2 = new LancheBuilder()
+    .definirTamanho("15 cm")
+    .escolherPao("Italiano")
+    .escolherProteina("Atum")
+    .escolherQueijo("Mussarela")
+    .escolherSalada("Repolho")
+    .escolherMolho("Cebola Agridoce")
+    .construir();
+lanche2.mostrarPedido();
+
+const lanche3 = new LancheBuilder()
+    .definirTamanho("30 cm")
+    .escolherPao("Integral")
+    .escolherSalada("Picles")
+    .escolherMolho("Mostarda e Mel")
+    .construir();
+lanche3.mostrarPedido();
+
+const lanche4 = new LancheBuilder()
+    .definirTamanho("30 cm")
+    .escolherPao("Italiano")
+    .escolherProteina("Frango")
+    .escolherQueijo("Mussarela")
+    .escolherAquecido("Sim")
+    .escolherSalada("Alface")
+    .escolherMolho("Chipotle")
+    .construir();
+lanche4.mostrarPedido();
